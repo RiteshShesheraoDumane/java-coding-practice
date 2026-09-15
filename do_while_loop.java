18a2d99802a76bf0d55cd2b0aab7456eadf5ca77
Ritesh dumne <166941944+RiteshShesheraoDumane@users.noreply.github.com>
2026-09-15 17:21:42 +0530

    Add files via upload

---
+++ do_while_loop.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class do_while_loop {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter any number:");
+	int num=sc.nextInt();
+	do {
+		System.out.println("learn coding");
+		num++;
+		
+	}
+	while(num<=10);
+			
+			
+}
+}
