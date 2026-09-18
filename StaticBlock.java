653079a7afcc0aba78554f22f65b498a803fa161
Ritesh dumne <166941944+RiteshShesheraoDumane@users.noreply.github.com>
2026-09-18 11:12:22 +0530

    Add files via upload

---
+++ StaticBlock.java
@@ -0,0 +1,11 @@
+public class StaticBlock {
+
+    // Static block
+    static {
+        System.out.println("Learn Java");
+    }
+
+    public static void main(String[] args) {
+      // here no need to creating object because of static block
+    }
+}
