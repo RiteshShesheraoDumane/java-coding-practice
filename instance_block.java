5514f980d3e67f9c131930dee4fb4f4208a1e4be
Ritesh dumne <166941944+RiteshShesheraoDumane@users.noreply.github.com>
2026-09-18 11:08:34 +0530

    Add files via upload

---
+++ instance_block.java
@@ -0,0 +1,11 @@
+
+public class instance_block {
+	
+	 // Instance block
+	{
+		System.out.println(" learn coding");
+	}
+	public static void main(String[] args) {
+		instance_block obj =new instance_block();
+	}
+}
